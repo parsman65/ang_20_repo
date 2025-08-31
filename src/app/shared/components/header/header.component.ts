748bcc1e2f1818b6738a05858a3ec79931ca90fd
User <alislm65@gmail.com>
2025-08-31 15:42:34 +0330

    add logo

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,13 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   standalone: true,
+  imports: [
+    TranslatePipe
+  ],
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
