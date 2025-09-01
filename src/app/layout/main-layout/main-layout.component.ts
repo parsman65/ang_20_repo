72dd30eb09012d40cb648354beb16859c3daa0c9
ali <alislm65@gmail.com>
2025-09-01 21:58:29 +0330

    fix p-carousel

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,13 +1,12 @@
 import {Component} from '@angular/core';
 import {HeaderComponent} from '../../shared/components/header/header.component';
-import {FooterComponent} from '../../shared/components/footer/footer.component';
 import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterOutlet]
+  imports: [HeaderComponent, RouterOutlet]
 })
 export class MainLayoutComponent {
 
