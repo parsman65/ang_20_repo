ea1445aed29c485c21aca2a4dabdca9ce74955f3
User <alislm65@gmail.com>
2025-09-11 10:54:11 +0330

    add footer

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -5,15 +5,16 @@ import {CourseCardComponent} from '../courses/course-card/course-card.component'
 import {NgForOf, NgStyle} from '@angular/common';
 import {Carousel} from 'primeng/carousel';
 import {Tag} from 'primeng/tag';
+import {FooterComponent} from "../../shared/components/footer/footer.component";
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  imports: [
-    TranslatePipe, ButtonModule, CourseCardComponent, NgForOf, Carousel, Tag, NgStyle
-  ],
+    imports: [
+        TranslatePipe, ButtonModule, CourseCardComponent, NgForOf, Carousel, Tag, NgStyle, FooterComponent
+    ],
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
